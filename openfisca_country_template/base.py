3f20368f784336e1fe413ab60971a8de84bcbb4c
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-03-14 11:29:36 +0100

    Update base.py

--- openfisca_country_template/base.py
+++ openfisca_country_template/base.py
@@ -3,7 +3,7 @@
 # All the objects defined or imported here will be available in all your legislation files
 
 # Import from openfisca-core the common python objects used to code the legislation in OpenFisca
-from openfisca_core.formula_toolbox import *
+from openfisca_core.model_api import *
 
 # Import the entities specifically defined for this tax and benefit system
 from openfisca_country_template.entities import Household, Person
