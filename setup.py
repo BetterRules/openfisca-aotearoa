cb3810e954809bcda6a01df8f36164eba449571f
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-03-24 12:03:02 +0100

    Upgrade OpenFisca Core

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     license='http://www.fsf.org/licensing/licenses/agpl-3.0.html',
     include_package_data = True,  # Will read MANIFEST.in
     install_requires=[
-        'OpenFisca-Core >= 6.1.0, < 8.0',
+        'OpenFisca-Core >= 6.1.0, < 10.0',
         ],
     packages=find_packages(),
     test_suite='nose.collector',
