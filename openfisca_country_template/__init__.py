dc2c5d2f02e197c83edd68ba89a1f4f2e429d571
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-04-24 15:01:12 +0200

    Split parameter file

--- openfisca_country_template/__init__.py
+++ openfisca_country_template/__init__.py
@@ -8,7 +8,6 @@ from . import entities
 
 
 COUNTRY_DIR = os.path.dirname(os.path.abspath(__file__))
-PARAM_FILE = os.path.join(COUNTRY_DIR, 'parameters.xml')
 
 # Our country tax and benefit class, which inherits from the general TaxBenefitSystem class
 # The name CountryTaxBenefitSystem must not be changed, as all tools of the OpenFisca ecosystem expect a CountryTaxBenefitSystem class to be exposed in the __init__ module of a country package
@@ -20,5 +19,14 @@ class CountryTaxBenefitSystem(TaxBenefitSystem):
         # We add to our tax and benefit system all the variables
         self.add_variables_from_directory(os.path.join(COUNTRY_DIR, 'variables'))
 
-        # We add to our tax and benefit system all the legislation parameters defined in parameters.xml
-        self.add_legislation_params(PARAM_FILE)
+        # We add to our tax and benefit system all the legislation parameters defined in the  parameters files
+        param_files = [
+            '__root__.xml',
+            'benefit.xml',
+            'general.xml',
+            'tax.xml',
+            ]
+
+        for param_file in param_files:
+            param_path = os.path.join(COUNTRY_DIR, 'parameters', param_file)
+            self.add_legislation_params(param_path)
