9a791db6d167d01b0ea752722f770be687341415
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-04-24 17:03:12 +0200

    Add BAREME exemple

--- openfisca_country_template/variables/benefit.py
+++ openfisca_country_template/variables/benefit.py
@@ -16,13 +16,13 @@ class basic_income(DatedVariable):
     # Since Dec 1st 2016, the basic income is provided to any adult, without cousidering their income.
     @dated_function(start = date(2016, 12, 1))
     def function_from_2016_12(person, period, legislation):
-        age_condition = person('age', period) >= legislation(period).age_of_majority
-        return age_condition * legislation(period).basic_income  # This '*' is a vectorial 'if'. See https://doc.openfisca.fr/coding-the-legislation/30_case_disjunction.html#simple-multiplication
+        age_condition = person('age', period) >= legislation(period).general.age_of_majority
+        return age_condition * legislation(period).benefit.basic_income  # This '*' is a vectorial 'if'. See https://doc.openfisca.fr/coding-the-legislation/30_case_disjunction.html#simple-multiplication
 
     # From Dec 1st 2015 to Nov 30 2016, the basic income is provided to adults who have no income.
     # Before Dec 1st 2015, the basic income does not exist in the law, and calculating it returns its default value, which is 0.
     @dated_function(start = date(2015, 12, 1), stop = date(2016, 11, 30))
     def function_until_2016_12(person, period, legislation):
-        age_condition = person('age', period) >= legislation(period).age_of_majority
+        age_condition = person('age', period) >= legislation(period).general.age_of_majority
         salary_condition = person('salary', period) == 0
-        return age_condition * salary_condition * legislation(period).basic_income  # The '*' is also used as a vectorial 'and'. See https://doc.openfisca.fr/coding-the-legislation/25_vectorial_computing.html#forbidden-operations-and-alternatives
+        return age_condition * salary_condition * legislation(period).benefit.basic_income  # The '*' is also used as a vectorial 'and'. See https://doc.openfisca.fr/coding-the-legislation/25_vectorial_computing.html#forbidden-operations-and-alternatives
--- openfisca_country_template/variables/tax.py
+++ openfisca_country_template/variables/tax.py
@@ -15,4 +15,18 @@ class income_tax(Variable):
 
     # The formula to compute the income tax for a given person at a given period
     def function(person, period, legislation):
-        return person('salary', period) * legislation(period).income_tax_rate
+        return person('salary', period) * legislation(period).tax.income_tax_rate
+
+
+class social_security_contribution(Variable):
+    column = FloatCol
+    entity = Person
+    definition_period = MONTH
+    label = u"Progressive contribution paid on salaries to finance social security"
+    url = "https://law.gov.example/social_security_contribution"  # Always use the most official source
+
+    def function(person, period, legislation):
+        salary = person('salary', period)
+        scale = legislation(period).tax.social_security_contribution
+
+        return scale.calc(salary)
