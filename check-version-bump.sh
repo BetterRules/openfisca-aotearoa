8d37a5db061f27e6faa2eb2d6f5dae342746d4e9
Matti Schneider <matti.schneider@beta.gouv.fr>
2017-05-17 19:22:32 +0200

    Improve and simplify version bump check
    Exit with different exit codes depending on the type of error

--- check-version-bump.sh
+++ check-version-bump.sh
@@ -2,19 +2,24 @@
 
 current_version=`python setup.py --version`
 
-if ! git diff-index --quiet origin/master openfisca_country_template
-then
-    if git rev-parse $current_version
-    then
-        set +x
-        echo "Version $current_version already exists. Please update version number in setup.py before merging this branch into master."
-        exit 1
-    fi
+if git diff-index --quiet origin/master openfisca_country_template
+then exit 0  # there are no changes at all, the version is correct
+fi
 
-    if git diff-index origin/master --quiet CHANGELOG.md
-    then
-        set +x
-        echo "CHANGELOG.md has not been modified. Please update it before merging this branch into master."
-        exit 1
-    fi
+if git rev-parse --verify --quiet $current_version
+then
+    echo "Version $current_version already exists:"
+    git log -1 $current_version
+    echo
+    echo "Update the version number in setup.py before merging this branch into master."
+    echo "Look at the CONTRIBUTING.md file to learn how the version number should be updated."
+    exit 1
+fi
+
+if git diff-index --quiet origin/master CHANGELOG.md
+then
+    echo "CHANGELOG.md has not been modified, while the code has changed."
+    echo "Explain what you changed before merging this branch into master."
+    echo "Look at the CONTRIBUTING.md file to learn how to write the changelog."
+    exit 2
 fi
