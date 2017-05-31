7f77b8aff609663a4b67378e7ab8aad95f83ca79
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-05-31 21:59:51 +0200

    Adapt country template to 13

--- openfisca_country_template/reforms/flat_social_security_contribution.py
+++ openfisca_country_template/reforms/flat_social_security_contribution.py
@@ -13,7 +13,7 @@ from openfisca_core.model_api import *
 class social_security_contribution(Variable):
     # Variable metadata don't need to be redefined. By default, the reference variable metadatas will be used.
 
-    def function(person, period, legislation):
+    def formula(person, period, legislation):
         return person('salary', period) * 0.03
 
 
