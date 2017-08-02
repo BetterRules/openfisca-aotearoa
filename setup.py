14ed724aecb3f7dfa59e7c63c7c464c5afc994a9
Michel Blancard <michel.blancard@data.gouv.fr>
2017-08-02 15:20:04 +0200

    Update Changelog

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='OpenFisca-Country-Template',
-    version='1.2.4',
+    version='1.2.5',
     author='OpenFisca Team',
     author_email='contact@openfisca.fr',
     description=u'OpenFisca tax and benefit system for Country-Template',
