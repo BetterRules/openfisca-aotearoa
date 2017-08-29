a6eb468d8e4342fcc498f76820ff08e11e2cc3c8
sandcha <sandcha@users.noreply.github.com>
2017-08-29 15:55:43 +0200

    Remove indentations from entities doc attribute

--- openfisca_country_template/entities.py
+++ openfisca_country_template/entities.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # This file defines the entities needed by our legislation.
+import textwrap
 
 from openfisca_core.entities import build_entity
 
@@ -8,12 +9,12 @@ Household = build_entity(
     key = "household",
     plural = "households",
     label = u'Household',
-    doc = '''
+    doc = textwrap.dedent('''
     A group entity.
     Contains multiple natural persons with specific roles.
     From zero to two parents with 'first_parent' and 'second_parent' subroles.
     And an unlimited number of children.
-    ''',
+    '''),
     roles = [
         {
             'key': 'parent',
@@ -35,10 +36,10 @@ Person = build_entity(
     key = "person",
     plural = "persons",
     label = u'Person',
-    doc = '''
+    doc = textwrap.dedent('''
     The minimal legal entity on which a legislation might be applied.
     Represents a natural person.
-    ''',
+    '''),
     is_person = True,
     )
 
