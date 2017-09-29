92f0763e7bc1e5a159fc95b54cb407d1d55612bf
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-09-29 15:00:01 +0200

    Add JSON example

---
+++ openfisca_country_template/situation_examples/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import json
+import os
+
+
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+def parse(file_name):
+    file_path = os.path.join(DIR_PATH, file_name)
+    with open(file_path, 'r') as file:
+        return json.loads(file.read())
+
+
+single = parse('single.json')
+couple = parse('couple.json')
