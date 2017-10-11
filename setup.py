58361a5671ca1e6df87867fea10164f0e4e7a48c
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-10-11 17:09:13 +0200

    Declare package compatible with OpenFisca Core v19

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='OpenFisca-Country-Template',
-    version='1.3.0',
+    version='1.3.1',
     author='OpenFisca Team',
     author_email='contact@openfisca.fr',
     description=u'OpenFisca tax and benefit system for Country-Template',
@@ -16,7 +16,7 @@ setup(
     url='https://github.com/openfisca/openfisca-country-template',
     include_package_data = True,  # Will read MANIFEST.in
     install_requires=[
-        'OpenFisca-Core >= 17.1.0, < 18.0',
+        'OpenFisca-Core >= 17.1.0, < 19.0',
         ],
     extras_require = {
         'api': [
