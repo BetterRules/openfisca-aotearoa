bb231ee3b6426cc4548de0c66a77a81e15f3a43b
Florian Pagnoux <florian.pagnoux@gmail.com>
2017-12-15 11:44:02 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='OpenFisca-Country-Template',
-    version='2.0.0',
+    version='2.0.1',
     author='OpenFisca Team',
     author_email='contact@openfisca.fr',
     description=u'OpenFisca tax and benefit system for Country-Template',
@@ -16,7 +16,7 @@ setup(
     url='https://github.com/openfisca/openfisca-country-template',
     include_package_data = True,  # Will read MANIFEST.in
     install_requires=[
-        'OpenFisca-Core >= 21.0.0, < 22.0',
+        'OpenFisca-Core >= 21.0.2, < 22.0',
         ],
     extras_require = {
         'api': [
