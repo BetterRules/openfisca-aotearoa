924035b714fef3f3b6f3922c131cfc658cdbe175
Hamish Fraser <hamish@verb.co.nz>
2018-05-03 15:19:30 +1200

    Identify the parameters and variables relating to income-tax, specifically to start the process of creating the coding standard for handling NZ legislation

--- openfisca_aotearoa/variables/income.py
+++ openfisca_aotearoa/variables/income.py
@@ -10,6 +10,88 @@ from openfisca_core.model_api import *
 from openfisca_aotearoa.entities import *
 
 
+class TaxPayerFilingStatus(Enum):
+    __order__ = "non_filing filing filing_with_schedular_income"
+    non_filing = u'Non-filing taxpayer'
+    filing = u'Filing taxpayer'
+    filing_with_schedular_income = u'Filing taxpayer with schedular income'
+
+
+class tax_payer_filing_status(Variable):
+    value_type = Enum
+    possible_values = TaxPayerFilingStatus
+    default_value = TaxPayerFilingStatus.non_filing
+    entity = Person
+    definition_period = YEAR
+    label = u""
+    reference = "http://www.legislation.govt.nz/act/public/2007/0097/latest/DLM1512331.html"
+
+
+class annual_gross_income(Variable):
+    value_type = float
+    entity = Person
+    definition_period = YEAR
+    label = "Annual gross income"
+    reference = "http://www.legislation.govt.nz/act/public/2007/0097/latest/DLM1512333.html"
+
+
+class annual_total_deduction(Variable):
+    value_type = float
+    entity = Person
+    definition_period = YEAR
+    label = "Annual total deduction"
+    reference = "http://www.legislation.govt.nz/act/public/2007/0097/latest/DLM1512336.html"
+
+
+class net_income(Variable):
+    value_type = float
+    entity = Person
+    definition_period = YEAR
+    label = "Net income"
+    reference = "http://www.legislation.govt.nz/act/public/2007/0097/latest/DLM1512339.html"
+
+    def formula(person, period, parameters):
+        net_income_calc = person('annual_gross_income', period) - person('annual_total_deduction', period)
+
+        return (
+            net_income_calc * (net_income_calc > 0)
+            )
+
+
+class net_loss(Variable):
+    value_type = float
+    entity = Person
+    definition_period = YEAR
+    label = "Net loss"
+    reference = "http://www.legislation.govt.nz/act/public/2007/0097/latest/DLM1512339.html"
+
+    def formula(person, period, parameters):
+        net_income_calc = person('annual_gross_income', period) - person('annual_total_deduction', period)
+
+        return (
+            net_income_calc * (net_income_calc < 0)
+            )
+
+
+class taxable_income(Variable):
+    value_type = float
+    entity = Person
+    definition_period = YEAR
+    label = "Net loss"
+    reference = "http://www.legislation.govt.nz/act/public/2007/0097/latest/DLM1512344.html"
+
+    def formula(person, period, parameters):
+        return (person('net_income', period) - person('available_tax_loss', period))
+
+
+class available_tax_loss(Variable):
+    value_type = float
+    entity = Person
+    definition_period = YEAR
+    label = "Available tax loss"
+    reference = "http://www.legislation.govt.nz/act/public/2007/0097/latest/DLM1520575.html#DLM1520774"
+
+
 # This variable is a pure input: it doesn't have a formula
 class salary(Variable):
     value_type = float
@@ -20,6 +102,7 @@ class salary(Variable):
     reference = "https://law.gov.example/salary"  # Always use the most official source
 
 
+# Openfisca example function, to be removed
 class disposable_income(Variable):
     value_type = float
     entity = Person
