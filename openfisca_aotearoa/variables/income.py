38fe4895cf4c28f121cdeb45f76a9538439ddc81
Matti Schneider <labplus@matti.consulting>
2018-05-09 16:01:33 +1200

    Remove unused template variables and tests

--- openfisca_aotearoa/variables/income.py
+++ openfisca_aotearoa/variables/income.py
@@ -18,21 +18,3 @@ class salary(Variable):
     set_input = set_input_divide_by_period  # Optional attribute. Allows user to declare a salary for a year. OpenFisca will spread the yearly amount over the months contained in the year.
     label = "Salary"
     reference = "https://law.gov.example/salary"  # Always use the most official source
-
-
-class disposable_income(Variable):
-    value_type = float
-    entity = Person
-    definition_period = MONTH
-    label = "Actual amount available to the person at the end of the month"
-    reference = "https://stats.gov.example/disposable_income"  # Some variables represent quantities used in economic models, and not defined by law. Always give the source of your definition.
-
-    def formula(person, period, parameters):
-        return (
-            + person('salary', period)
-            + person('basic_income', period)
-            - person('income_tax', period)
-            - person('social_security_contribution', period)
-            )
-
-# This file is from the OpenFisca default country template and as such can be removed
