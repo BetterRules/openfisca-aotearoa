2eb1445639a2d3fd803bf6f9994c66000e14284b
Matti Schneider <labplus@matti.consulting>
2018-05-09 16:04:27 +1200

    Remove tutorial comments

--- openfisca_aotearoa/variables/income.py
+++ openfisca_aotearoa/variables/income.py
@@ -1,20 +1,14 @@
 # -*- coding: utf-8 -*-
 
-# This file defines the variables of our legislation.
-# A variable is property of a person, or an entity (e.g. a property).
-# See http://openfisca.org/doc/variables.html
-
 # Import from openfisca-core the common python objects used to code the legislation in OpenFisca
 from openfisca_core.model_api import *
 # Import the entities specifically defined for this tax and benefit system
 from openfisca_aotearoa.entities import *
 
 
-# This variable is a pure input: it doesn't have a formula
 class salary(Variable):
     value_type = float
     entity = Person
     definition_period = YEAR
-    set_input = set_input_divide_by_period  # Optional attribute. Allows user to declare a salary for a year. OpenFisca will spread the yearly amount over the months contained in the year.
+    set_input = set_input_divide_by_period  # Allows user to declare a salary for a year. OpenFisca will spread the yearly amount over the months contained in the year.
     label = "Salary"
-    reference = "https://law.gov.example/salary"  # Always use the most official source
