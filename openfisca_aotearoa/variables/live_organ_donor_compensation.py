0f810b572c9ea393af6f0a02b82798b4aa3f3d0b
Nigel Charman <nigel.charman.nz@gmail.com>
2018-05-19 17:13:04 +1200

    Applied rounding, commented out unneeded code

--- openfisca_aotearoa/variables/live_organ_donor_compensation.py
+++ openfisca_aotearoa/variables/live_organ_donor_compensation.py
@@ -3,8 +3,8 @@
 # Import from openfisca-core the common python objects used to code the legislation in OpenFisca
 from openfisca_core.model_api import *
 # Import the entities specifically defined for this tax and benefit system
-from openfisca_aotearoa.entities import Titled_Property, Person
-from numpy import clip, floor
+from openfisca_aotearoa.entities import Person
+from numpy import round
 
 class weekly_compensation_before_tax(Variable):
     value_type = float
@@ -12,27 +12,27 @@ class weekly_compensation_before_tax(Variable):
     definition_period = YEAR # TODO - determine whether we need to get WEEK to work
     label = u"The amount payable as compensation per week before tax"
     def formula(persons, period, parameters):
-        return persons('sum_of_earnings_in_last_52_weeks', period) / persons('earnings_period_in_weeks', period)
+        return round(persons('sum_of_earnings_in_last_52_weeks', period) / persons('earnings_period_in_weeks', period), 2)
 
-class PayFrequency(Enum):
-    weekly = u'Weekly'
-    fortnightly = u'Fortnightly'
-    four_weekly = u'Four Weekly'
-    monthly = u'Monthly'
+# class PayFrequency(Enum):
+#     weekly = u'Weekly'
+#     fortnightly = u'Fortnightly'
+#     four_weekly = u'Four Weekly'
+#     monthly = u'Monthly'
 
-class pay_frequency(Variable):
-    value_type = Enum
-    possible_values = PayFrequency
-    default_value = PayFrequency.weekly  # The default is mandatory
-    entity = Person
-    definition_period = YEAR
-    label = u"The frequency at which a person is paid"
+# class pay_frequency(Variable):
+#     value_type = Enum
+#     possible_values = PayFrequency
+#     default_value = PayFrequency.weekly  # The default is mandatory
+#     entity = Person
+#     definition_period = YEAR
+#     label = u"The frequency at which a person is paid"
 
-class salary_per_pay(Variable):
-    value_type = float
-    entity = Person
-    definition_period = YEAR # TODO - determine whether we need to get WEEK to work
-    label = u"The salary earned by a person before tax"
+# class salary_per_pay(Variable):
+#     value_type = float
+#     entity = Person
+#     definition_period = YEAR # TODO - determine whether we need to get WEEK to work
+#     label = u"The salary earned by a person before tax"
 
 class kiwisaver_member(Variable):
     value_type = bool
