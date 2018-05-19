090f12dd8004fe3994541429af36ae1bac0c30a3
Nigel Charman <nigel.charman.nz@gmail.com>
2018-05-19 15:03:00 +1200

    Tidy up

--- openfisca_aotearoa/variables/live_organ_donor_compensation.py
+++ openfisca_aotearoa/variables/live_organ_donor_compensation.py
@@ -11,29 +11,8 @@ class weekly_compensation_before_tax(Variable):
     entity = Person
     definition_period = YEAR # TODO - determine whether we need to get WEEK to work
     label = u"The amount payable as compensation per week before tax"
-
     def formula(persons, period, parameters):
-        # income_threshold = parameters(period).benefits.rates_rebates.income_threshold
-        # additional_per_dependant = parameters(period).benefits.rates_rebates.additional_per_dependant
-        # initial_contribution = parameters(period).benefits.rates_rebates.initial_contribution
-        # maximum_allowable = parameters(period).benefits.rates_rebates.maximum_allowable
-        # maximum_allowable = 10000000.00
-
-        # # sum allowable income including all the dependants for property
-        # allowable_income = (titled_properties.sum(titled_properties.members('dependants_as_per_rates_rebates', period)) * additional_per_dependant) + income_threshold
-
-        # # wrapping floor math function is non legislative and only to conform output of variable with existing infrastracture.
-        # excess_income = floor((titled_properties.sum(titled_properties.members('combined_income_as_per_rates_rebates', period)) - allowable_income) / 8)
-
-        # # minus the initial contribution
-        # rates_minus_contribution = titled_properties('rates_total_as_per_rates_rebates', period) - initial_contribution
-
-        # # perform the calculation
-        # rebate = rates_minus_contribution - ((rates_minus_contribution / 3) + excess_income)
-
-        # # Ensures the results aren't negative (less than 0) or greater than the maximum_allowable
         return persons('salary_per_pay', period)
-        # return clip(1200.00, 0, 10000000)
 
 class PayFrequency(Enum):
     weekly = u'Weekly'
