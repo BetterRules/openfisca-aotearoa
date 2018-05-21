0c7ee37e66895967f4021d53986274e154918360
Nigel Charman <nigel.charman.nz@gmail.com>
2018-05-21 20:37:16 +1200

    Removed commented out code

--- openfisca_aotearoa/variables/live_organ_donor_compensation.py
+++ openfisca_aotearoa/variables/live_organ_donor_compensation.py
@@ -55,26 +55,6 @@ class kiwisaver_employee_deduction(Variable):
         kiwisaver_employee_deduction_percentage = persons('kiwisaver_employee_deduction_percentage', period)
         return round(weekly_compensation * kiwisaver_employee_deduction_percentage / 100, 2)
 
-# class PayFrequency(Enum):
-#     weekly = u'Weekly'
-#     fortnightly = u'Fortnightly'
-#     four_weekly = u'Four Weekly'
-#     monthly = u'Monthly'
-
-# class pay_frequency(Variable):
-#     value_type = Enum
-#     possible_values = PayFrequency
-#     default_value = PayFrequency.weekly  # The default is mandatory
-#     entity = Person
-#     definition_period = YEAR
-#     label = u"The frequency at which a person is paid"
-
-# class salary_per_pay(Variable):
-#     value_type = float
-#     entity = Person
-#     definition_period = YEAR # TODO - determine whether we need to get WEEK to work
-#     label = u"The salary earned by a person before tax"
-
 class kiwisaver_member(Variable):
     value_type = bool
     default_value = True
