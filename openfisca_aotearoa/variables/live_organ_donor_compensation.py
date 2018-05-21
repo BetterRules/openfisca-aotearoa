82a8810ca178895c272a73a6802aa51ae1401ca5
Nigel Charman <nigel.charman.nz@gmail.com>
2018-05-21 21:20:36 +1200

    Fixed formatting so tests pass when using 'make test'

--- openfisca_aotearoa/variables/live_organ_donor_compensation.py
+++ openfisca_aotearoa/variables/live_organ_donor_compensation.py
@@ -6,6 +6,7 @@ from openfisca_core.model_api import *
 from openfisca_aotearoa.entities import Person
 from numpy import round
 
+
 class weekly_compensation_before_tax(Variable):
     """
     Calculate the weekly compensation based on employee earnings over the last 52 weeks
@@ -16,13 +17,13 @@ class weekly_compensation_before_tax(Variable):
     """
     value_type = float
     entity = Person
-    definition_period = YEAR # TODO - determine whether we need to get WEEK to work
+    definition_period = YEAR  # TODO - determine whether we need to get WEEK to work
     label = u"The amount payable as compensation per week before tax"
     # note that this also implements http://www.legislation.govt.nz/act/public/2016/0096/latest/whole.html?search=ts_act%40bill%40regulation%40deemedreg_live+organ_resel_25_a#DLM6965116
     reference = "http://www.legislation.govt.nz/act/public/2016/0096/latest/whole.html?search=ts_act%40bill%40regulation%40deemedreg_live+organ_resel_25_a#DLM6965123"
 
     def formula(persons, period, parameters):
-        earnings_amount = persons('sum_of_earnings_in_last_52_weeks', period) 
+        earnings_amount = persons('sum_of_earnings_in_last_52_weeks', period)
         compensation_amount = persons('sum_of_earnings_during_compensation_period_in_last_52_weeks', period)
         earnings_excluding_compensation_amount = earnings_amount - compensation_amount
 
@@ -35,13 +36,14 @@ class weekly_compensation_before_tax(Variable):
 
         employee_weekly_earnings = earnings_excluding_compensation_amount / earnings_excluding_compensation_period
         self_employed_weekly_earnings = self_employed_earnings_in_most_recent_tax_year / most_recent_tax_year_period
-        
+
         return round(employee_weekly_earnings + self_employed_weekly_earnings, 2)
+
 
 class kiwisaver_employee_deduction(Variable):
     value_type = float
     entity = Person
-    definition_period = YEAR # TODO - determine whether we need to get WEEK to work
+    definition_period = YEAR  # TODO - determine whether we need to get WEEK to work
     label = u"The amount deducted as a kiwisaver contribution"
     # Salary or wages includes a payment of earnings compensation under the Compensation for Live Organ Donors Act 2016;
     # Ref: http://www.legislation.govt.nz/act/public/2007/0097/latest/DLM1519947.html?search=ts_act%40bill%40regulation%40deemedreg_live+organ_resel_25_a#DLM1519947
@@ -49,7 +51,7 @@ class kiwisaver_employee_deduction(Variable):
     reference = "http://www.legislation.govt.nz/act/public/2006/0040/latest/whole.html?search=ts_act%40bill%40regulation%40deemedreg_live+organ_resel_25_a#DLM379040"
 
     def formula(persons, period, parameters):
-        earnings_amount = persons('sum_of_earnings_in_last_52_weeks', period) 
+        earnings_amount = persons('sum_of_earnings_in_last_52_weeks', period)
         compensation_amount = persons('sum_of_earnings_during_compensation_period_in_last_52_weeks', period)
         earnings_excluding_compensation_amount = earnings_amount - compensation_amount
 
@@ -62,11 +64,12 @@ class kiwisaver_employee_deduction(Variable):
 
         employee_weekly_earnings = earnings_excluding_compensation_amount / earnings_excluding_compensation_period
         self_employed_weekly_earnings = self_employed_earnings_in_most_recent_tax_year / most_recent_tax_year_period
-        
+
         weekly_compensation = round(employee_weekly_earnings + self_employed_weekly_earnings, 2)
 
         kiwisaver_employee_deduction_percentage = persons('kiwisaver_employee_deduction_percentage', period)
         return round(weekly_compensation * kiwisaver_employee_deduction_percentage / 100, 2)
+
 
 class kiwisaver_member(Variable):
     value_type = bool
@@ -75,12 +78,14 @@ class kiwisaver_member(Variable):
     label = u"Whether the person currently pays into Kiwisaver"
     definition_period = YEAR
 
+
 class sum_of_earnings_in_last_52_weeks(Variable):
     value_type = float
     entity = Person
     label = u"Total earnings over last 52 weeks"
     reference = "http://organiccoders.allengeer.com/brk-1234-abcdfcrg234356/"
     definition_period = YEAR
+
 
 class earnings_period_in_weeks(Variable):
     value_type = int
@@ -90,17 +95,20 @@ class earnings_period_in_weeks(Variable):
     reference = "http://organiccoders.allengeer.com/definitions-number-of-weeks/"
     definition_period = YEAR
 
+
 class sum_of_earnings_during_compensation_period_in_last_52_weeks(Variable):
     value_type = float
     entity = Person
     label = u"Earnings during the period in which the donor was entitled to weekly compensation (as defined in section 6(1) of the Accident Compensation Act 2001)"
     definition_period = YEAR
 
+
 class compensation_period_in_weeks(Variable):
     value_type = int
     entity = Person
     label = u"The number of weeks in which the donor was entitled to weekly compensation (as defined in section 6(1) of the Accident Compensation Act 2001) over the last 52 weeks"
     definition_period = YEAR
+
 
 class sum_of_self_employed_earnings_in_most_recently_completed_tax_year(Variable):
     value_type = float
@@ -109,12 +117,14 @@ class sum_of_self_employed_earnings_in_most_recently_completed_tax_year(Variable
     label = u"Self-employed earnings during the period in which the donor was entitled to weekly compensation (as defined in section 6(1) of the Accident Compensation Act 2001)"
     definition_period = YEAR
 
+
 class number_of_weeks_in_most_recently_completed_tax_year(Variable):
     value_type = int
     default_value = 52
     entity = Person
     label = u"The number of weeks in the most recently completed tax year"
     definition_period = YEAR
+
 
 class kiwisaver_employee_deduction_percentage(Variable):
     value_type = int
