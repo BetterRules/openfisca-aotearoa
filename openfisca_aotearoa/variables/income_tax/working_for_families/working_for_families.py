25f7cace433b0fbfb5c953f5aeea3bfe0aca2cf3
James Blair <mail@jamesblair.net>
2018-05-30 22:07:31 +1200

    Updates to best start tax credit entitlement variable

--- openfisca_aotearoa/variables/income_tax/working_for_families/working_for_families.py
+++ openfisca_aotearoa/variables/income_tax/working_for_families/working_for_families.py
@@ -3,7 +3,7 @@
 # Import from openfisca-core the common python objects used to code the legislation in OpenFisca
 from openfisca_core.model_api import *
 # Import the entities specifically defined for this tax and benefit system
-from openfisca_aotearoa.entities import Titled_Property, Person
+from openfisca_aotearoa.entities import Titled_Property, Person, Family
 from numpy import clip, floor
 
 class social_security__received_income_tested_benefit(Variable):
@@ -48,6 +48,14 @@ class income_tax__dependant(Variable):
         return age <= 18 
         # TODO - It's not this simple, this needs to be tweaked to include the edge criteria.
 
+class income_tax__income(Variable):
+        value_type = float
+        entity = Person
+        definition_period = YEAR
+        label = u'The annual income for a Person in a Family.'
+        reference = "" # TODO Add
+
+
 class income_tax__eligible_for_working_for_families(Variable):
     value_type = bool
     entity = Person
@@ -75,3 +83,36 @@ class income_tax__eligible_for_best_start_tax_credit(Variable):
         return True 
         # TODO - Base eligibility of ages of children as per legislation.
 
+
+class income_tax__entitlement_for_best_start_tax_credit(Variable):
+    value_type = float
+    entity = Family
+    definition_period = MONTH
+    label = u'Value of a Families entitlement for best start tax credit'
+    reference = "http://legislation.govt.nz/bill/government/2017/0004/15.0/DLM7512349.html"
+
+    def formula(family, period, parameters):
+
+                # sum up families income
+                family_income   = family.sum(family.members('income_tax__income', period.this_year))
+
+                # calculate income over the threshold
+                income_over_threshold = where((family_income - 79000) < 0, 0, family_income - 79000)
+
+                # work out the ages for each family member
+                ages = family.members('age', period)
+
+                # work out if each dependant child is eligible for full best start tax credit
+                dependant_eligible_full = ages == 0
+
+                # work out if each dependant child is eligible for abated best start tax credit
+                dependant_eligible_abated_1 = ages == 1
+                dependant_eligible_abated_2 = ages == 2
+
+                # sum up the entitlement for each child
+                bstc_entitlement = family.sum(dependant_eligible_full * 3120  
+                + dependant_eligible_abated_1 * (3120 - (income_over_threshold * .21))
+                + dependant_eligible_abated_2 * (3120 - (income_over_threshold * .21)))
+
+                return bstc_entitlement
+                # TODO - Replace hardcoded values with parameters already created.
