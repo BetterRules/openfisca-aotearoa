0702153a143ec996a2dbb0f2b09b0db26c86366d
Hamish <hamish@verb.co.nz>
2018-06-01 11:47:48 +1200

    remove unutilised references and excess whitespace

--- openfisca_aotearoa/variables/demographics/demographics.py
+++ openfisca_aotearoa/variables/demographics/demographics.py
@@ -3,8 +3,8 @@
 # Import from openfisca-core the common python objects used to code the legislation in OpenFisca
 from openfisca_core.model_api import *
 # Import the entities specifically defined for this tax and benefit system
-from openfisca_aotearoa.entities import Titled_Property, Person
-from numpy import clip, floor
+from openfisca_aotearoa.entities import Person
+
 
 # This variable is a pure input: it doesn't have a formula
 class birth(Variable):
@@ -14,6 +14,7 @@ class birth(Variable):
     label = u"Birth date"
     definition_period = ETERNITY  # This variable cannot change over time.
     reference = u"https://en.wiktionary.org/wiki/birthdate"
+
 
 class age(Variable):
     value_type = int
@@ -31,5 +32,3 @@ class age(Variable):
         is_birthday_past = (birth_month <= period.start.month) + (birth_month == period.start.month) * (birth_day <= period.start.day)
 
         return (period.start.year - birth_year) - where(is_birthday_past, 0, 1)  # If the birthday is not passed this year, substract one year
-
-
