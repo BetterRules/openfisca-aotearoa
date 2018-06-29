c8b708ab90caf564c07f0df8f73ec542067fa793
Hamish <hamish@verb.co.nz>
2018-06-29 13:46:32 +1200

    Switch pypi releases to be tag based

--- .circleci/deploy-if-version-bump.sh
+++ .circleci/deploy-if-version-bump.sh
@@ -2,8 +2,8 @@
 
 set -e
 if ! git rev-parse `python setup.py --version` 2>/dev/null ; then
-    git tag `python setup.py --version`
-    git push --tags  # update the repository version
+    # git tag `python setup.py --version`
+    # git push --tags  # update the repository version
     python setup.py bdist_wheel  # build this package in the dist directory
     twine upload dist/* --username $PYPI_USERNAME --password $PYPI_PASSWORD  # publish, these environment variables are set within the circleci interface
 else
