e52b4fcfef6fc569bd6e9a60fa45c9042019dd06
Brenda Wallace <shiny@cpan.org>
2018-08-10 10:12:54 +1200

    removed duplicate accomodation cash/income thresholds

--- openfisca_aotearoa/variables/social_security/accommodation_supplement.py
+++ openfisca_aotearoa/variables/social_security/accommodation_supplement.py
@@ -30,24 +30,10 @@ class social_security__eligible_for_accommodation_supplement(Variable):
         has_accomodation_costs = persons('has_accomodation_costs', period)
         not_social_housing = (persons('eligible_for_social_housing', period) == 0)
 
-        income = persons('income_below_threshold_for_accommodation_supplement', period)
-        cash = persons('cash_below_threshold_for_accommodation_supplement', period)
+        income = persons('accommodation_supplement__below_income_threshold', period)
+        cash = persons('accommodation_supplement__below_cash_threshold', period)
 
         return age_requirement * resident_or_citizen * in_nz * has_accomodation_costs * not_social_housing * income * cash
-
-
-class income_below_threshold_for_accommodation_supplement(Variable):
-    value_type = bool
-    entity = Person
-    label = u"Income is below threshold"
-    definition_period = MONTH
-
-
-class cash_below_threshold_for_accommodation_supplement(Variable):
-    value_type = bool
-    entity = Person
-    label = u"Cash is below threshold"
-    definition_period = MONTH
 
 
 class has_accomodation_costs(Variable):
