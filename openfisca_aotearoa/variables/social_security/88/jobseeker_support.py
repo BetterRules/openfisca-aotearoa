4bfa07844a6fceb7e7b69a8397563fbfc92b5550
Brenda Wallace <shiny@cpan.org>
2018-08-21 13:48:48 +1200

    Adding scenario of jobseeker for 18 and 19

--- openfisca_aotearoa/variables/social_security/88/jobseeker_support.py
+++ openfisca_aotearoa/variables/social_security/88/jobseeker_support.py
@@ -29,12 +29,25 @@ class jobseeker_support__meets_age_threshold(Variable):
     entity = Person
     label = u"Meets the age test for Jobseeker Support?"
     definition_period = MONTH
-    reference = "TODO"
+    reference = """
+    An applicant for jobseeker support
+        (a) must be aged at least 18 years, in the case of an applicant without a dependent child:
+        (b) must be aged at least 20 years, in any other case.
+    """
 
     def formula(persons, period, parameters):
-        # old enough?
-        age_threshold = parameters(period).entitlements.social_security.jobseeker_support.age_threshold
-        return persons("age", period) >= age_threshold
+        # over the simpler age threshold
+        jobseeker_age = parameters(period).entitlements.social_security.jobseeker_support.age_threshold
+        over_age_threshold = persons("age", period) >= jobseeker_age
+
+        # over the threshold for appliants with a dependent child
+        jobseeker_age_with_dependent_child = parameters(period).entitlements.social_security.jobseeker_support.age_threshold_with_dependent_child
+        has_dependent_child = persons('social_security__has_dependant_child', period)
+        over_age_threshold_with_dependent_child = (persons("age", period) >= jobseeker_age_with_dependent_child) * has_dependent_child
+
+        print(over_age_threshold_with_dependent_child)
+
+        return over_age_threshold + over_age_threshold_with_dependent_child
 
 
 class jobseeker_support__meets_years_in_nz_requirement(Variable):
@@ -57,6 +70,21 @@ class social_security__eligible_for_jobseeker_support(Variable):
     entity = Person
     definition_period = MONTH
     label = "Eligible for Job Seeker Support"
+    reference = """
+    88B Jobseeker support: standard eligibility requirements
+        (1) A person is entitled to jobseeker support if he or she satisfies the criteria in subsections (2), (3), and (4), and
+        (a) is not in full-time employment, but
+            (i) is seeking it; and
+            (ii) is available for it; and
+            (iii) is willing and able to undertake it; and
+            (iv) has taken reasonable steps to find it; or
+        (b) is not in full-time employment, but would comply with subparagraphs (i) to (iv) of paragraph (a) but for circumstances that would qualify the person
+            for an exemption under section 105 from some or all work test obligations; or
+        (c) is not in full-time employment and is willing to undertake it but, because of sickness, injury, or disability, is limited in his or her capacity to
+            seek, undertake, or be available for it; or
+        (d) is in employment, but is losing earnings because, through sickness or injury, he or she is not working at all, or is working only at a
+            reduced level.
+        """
 
     def formula(persons, period, parameters):
         # The applicant
@@ -64,6 +92,7 @@ class social_security__eligible_for_jobseeker_support(Variable):
         resident_or_citizen = persons('is_citizen_or_resident', period)
 
         years_in_nz = persons('jobseeker_support__meets_years_in_nz_requirement', period)
+
         age_requirement = persons('jobseeker_support__meets_age_threshold', period)
 
         # income low enough?
---
+++ openfisca_aotearoa/variables/social_security/interpretation/child.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Import from openfisca-core the common python objects used to code the legislation in OpenFisca
+from openfisca_core.model_api import *
+# Import the entities specifically defined for this tax and benefit system
+from openfisca_aotearoa.entities import Person
+
+
+class social_security__has_dependant_child(Variable):
+    value_type = bool
+    entity = Person
+    label = u"has a dependent child"
+    definition_period = MONTH
+
+
+class social_security__is_a_child(Variable):
+    value_type = bool
+    entity = Person
+    label = u"""child means a single person under the age of 18 years, other than a person who is—
+        (a) aged 16 years or 17 years; and
+        (b) financially independent
+        """
+    definition_period = MONTH
+    reference = u""
+
+    def formula(persons, period, parameters):
+        under_16 = persons('age', period) < 16
+        under_18 = persons('age', period) < 18
+
+        financially_independent = persons('social_security__is_financially_independent', period)
+
+        return under_16 + (under_18 * not_(financially_independent))
