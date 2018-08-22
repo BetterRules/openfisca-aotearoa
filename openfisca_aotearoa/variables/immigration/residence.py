385a982a0b2e00c415c5bbef0770b07e71b7c3ae
Brenda Wallace <shiny@cpan.org>
2018-08-23 11:55:43 +1200

    Adding Residence criteria from Immigration Act

--- openfisca_aotearoa/variables/immigration/residence.py
+++ openfisca_aotearoa/variables/immigration/residence.py
@@ -4,12 +4,29 @@ from openfisca_core.model_api import *
 from openfisca_aotearoa.entities import Person
 
 
+class immigration__holds_resident_visa(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = u"Holder of a resident visa"
+
+
+class immigration__holds_permanent_resident_visa(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = ETERNITY
+    label = u"Holder of a permanent resident visa"
+
+
 class is_resident(Variable):
     value_type = bool
     entity = Person
     definition_period = ETERNITY
     label = u"Holder of a permanent resident visa or a resident visa"
     reference = "Immigration Act 2009 (interpretation) http://legislation.govt.nz/act/public/2009/0051/latest/whole.html#DLM1440311"
+
+    def formula(persons, period, parameters):
+        return persons('immigration__holds_resident_visa', period) + persons('immigration__holds_permanent_resident_visa', period)
 
 
 class is_permanent_resident(Variable):
@@ -44,3 +61,17 @@ class is_citizen_or_resident(Variable):
 
     def formula(persons, period, parameters):
         return persons('is_nz_citizen', period) + persons('is_permanent_resident', period) + persons('is_resident', period)
+
+
+class immigration__is_recognised_refugee(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = u"is recognised as a refugee"
+
+
+class immigration__is_protected_person(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = u"is recognised as a a protected person in New Zealand"
--- openfisca_aotearoa/variables/social_security/88/jobseeker_support.py
+++ openfisca_aotearoa/variables/social_security/88/jobseeker_support.py
@@ -1,18 +1,6 @@
 from openfisca_aotearoa.entities import Person
 from openfisca_core.model_api import *
 
-# """
-# Benefit: JobSeeker Support (eligibility):
-#     If applicant.employmentStatus != "full-time"
-#         and 18 <= applicant.Age
-#         and applicant.isNZResident
-#         and applicant.hasLivedInNZfor2Years
-#         and applicant.normallyLivesInNZ
-#         and recipient.prepareForEmployment
-#         and threshold.income.JobSeekerSupport
-#     then benefit.isJobSeekerSupport is PERMITTED
-# """
-
 
 class jobseeker_support__is_prepared_for_employment(Variable):
     value_type = bool
@@ -30,9 +18,11 @@ class jobseeker_support__meets_age_threshold(Variable):
     label = u"Meets the age test for Jobseeker Support?"
     definition_period = MONTH
     reference = """
+    88B Jobseeker support: standard eligibility requirements
     An applicant for jobseeker support
         (a) must be aged at least 18 years, in the case of an applicant without a dependent child:
         (b) must be aged at least 20 years, in any other case.
+    http://legislation.govt.nz/act/public/1964/0136/latest/whole.html#DLM5478527
     """
 
     def formula(persons, period, parameters):
@@ -46,21 +36,6 @@ class jobseeker_support__meets_age_threshold(Variable):
         over_age_threshold_with_dependent_child = (persons("age", period) >= jobseeker_age_with_dependent_child) * has_dependent_child
 
         return over_age_threshold + over_age_threshold_with_dependent_child
-
-
-class jobseeker_support__meets_years_in_nz_requirement(Variable):
-    value_type = bool
-    default_value = True
-    entity = Person
-    label = u"Meets the Jobseeker Support test for number of years lived in nz"
-    definition_period = MONTH
-    reference = "TODO"
-
-    def formula(persons, period, parameters):
-        # been in NZ log enough?
-        min_years = parameters(period).entitlements.social_security.jobseeker_support.minumum_years_in_nz
-        years_in_nz = persons('number_of_years_lived_in_nz', period)
-        return years_in_nz >= min_years
 
 
 class social_security__eligible_for_jobseeker_support(Variable):
@@ -86,10 +61,7 @@ class social_security__eligible_for_jobseeker_support(Variable):
 
     def formula(persons, period, parameters):
         # The applicant
-        in_nz = persons('normally_lives_in_nz', period)
-        resident_or_citizen = persons('is_citizen_or_resident', period)
-
-        years_in_nz = persons('jobseeker_support__meets_years_in_nz_requirement', period)
+        residency_requirements = persons('social_security__meets_residential_requirements_for_certain_benefits', period)
 
         age_requirement = persons('jobseeker_support__meets_age_threshold', period)
 
@@ -99,5 +71,4 @@ class social_security__eligible_for_jobseeker_support(Variable):
         # Prepared to work
         prepared = persons('jobseeker_support__is_prepared_for_employment', period)
 
-        return in_nz * resident_or_citizen * \
-            age_requirement * income * prepared * years_in_nz
+        return age_requirement * income * prepared * residency_requirements
