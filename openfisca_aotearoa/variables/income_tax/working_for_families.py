b44eec0bf361dfe15f89d959974fd17abee0e545
Brenda Wallace <shiny@cpan.org>
2018-08-24 13:04:47 +1200

    Moved income tax variables to their own folder

--- openfisca_aotearoa/variables/income_tax/working_for_families.py
+++ openfisca_aotearoa/variables/income_tax/working_for_families.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# Import from openfisca-core the common python objects used to code the legislation in OpenFisca
 from openfisca_core.model_api import *
-# Import the entities specifically defined for this tax and benefit system
 from openfisca_aotearoa.entities import Person, Family
 
 
+# TODO!!! Move this
 class social_security__received_income_tested_benefit(Variable):
     value_type = bool
     entity = Person
