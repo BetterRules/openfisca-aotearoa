c7f1aaae78ee74094d9a22ad674e34a3565c9132
Brenda Wallace <shiny@cpan.org>
2018-08-29 16:48:41 +1200

    Bumping up open fisca version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     url='https://github.com/ServiceInnovationLab/openfisca-aotearoa',
     include_package_data=True,  # Will read MANIFEST.in
     install_requires=[
-        'OpenFisca-Core >= 22, < 23.0',
+        'OpenFisca-Core >= 24, < 25.0',
         ],
     extras_require={
         'api': [
