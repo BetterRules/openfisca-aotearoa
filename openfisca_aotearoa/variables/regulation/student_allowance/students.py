e0e12e3f7014602aa30f977f6324c39068080451
Brenda Wallace <shiny@cpan.org>
2018-09-06 09:32:52 +1200

    Added reference to Secondary student and tertiary student

--- openfisca_aotearoa/variables/regulation/student_allowance/students.py
+++ openfisca_aotearoa/variables/regulation/student_allowance/students.py
@@ -8,11 +8,7 @@ class student_allowance__is_tertiary_student(Variable):
     value_type = bool
     entity = Person
     label = u"a tertiary student"
-    reference = """
-    tertiary provider means a university, polytechnic, college of education, or
-    wananga, a private training establishment continued by the Act, or any other
-    education provider approved by the chief executive of the Ministry of Education for the purpose
-    """
+    reference = "legislation.govt.nz/regulation/public/1998/0277/latest/whole.html#DLM259980"
     definition_period = MONTH
     default_value = False
 
@@ -21,13 +17,6 @@ class student_allowance__is_secondary_student(Variable):
     value_type = bool
     entity = Person
     label = u"a secondary student"
-    reference = """
-    secondary school means
-    (a) a secondary school established under Part 12 of the Act or registered under section 35A of the Act; or
-    (b) a school that provides secondary instruction and is a correspondence school under section 145 of the
-        Act or is designated as a correspondence school under section 152 of the Act; or
-    (c) a special school that provides secondary instruction and is continued by Part 3 of the Education Act
-        1964, or a specified institution in Schedule 5 of the Act
-    """
+    reference = "http://legislation.govt.nz/regulation/public/1998/0277/latest/whole.html#DLM259943"
     definition_period = MONTH
     default_value = False
