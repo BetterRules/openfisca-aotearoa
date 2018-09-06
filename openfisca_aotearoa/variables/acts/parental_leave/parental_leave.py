008fe03ee4405165d3af7d8eca571a362f190011
Dana Iti <dana.iti.work@gmail.com>
2018-09-06 16:04:21 +1200

    added formula

--- openfisca_aotearoa/variables/acts/parental_leave/parental_leave.py
+++ openfisca_aotearoa/variables/acts/parental_leave/parental_leave.py
@@ -9,7 +9,7 @@ from openfisca_aotearoa.entities import Person
 class parental_leave__is_primary_carer(Variable):
     value_type = bool
     entity = Person
-    definition_period = YEAR
+    definition_period = MONTH
     label = u"Is primary carer"
     reference = "http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM120458.html?search=qs_act%40bill%40regulation%40deemedreg_parental+leave_resel_25_h&p=1#DLM120458"
 
@@ -17,7 +17,7 @@ class parental_leave__is_primary_carer(Variable):
 class parental_leave__applied_for_leave_or_stopped_working(Variable):
     value_type = bool
     entity = Person
-    definition_period = YEAR
+    definition_period = MONTH
     label = u"Has applied for/taken leave or stopped working immediately"
     reference = "http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM121773.html?search=sw_096be8ed8179b676_immediately_25_se&p=1"
 
@@ -25,7 +25,7 @@ class parental_leave__applied_for_leave_or_stopped_working(Variable):
 class parental_leave__passes_12_month_employment_test(Variable):
     value_type = bool
     entity = Person
-    definition_period = YEAR
+    definition_period = MONTH
     label = u"Number of Persons classified as dependant for the purposes of rates rebates"
     reference = """
     http://www.legislation.govt.nz/act/public/1987/0129/latest/link.aspx?search=qs_act%40bill%40regulation%40deemedreg_parental+leave_resel_25_h&p=1&id=DLM6810651#DLM6810651
@@ -40,14 +40,21 @@ class parental_leave__passes_12_month_employment_test(Variable):
 class parental_leave__had_previous_parental_leave_in_last_six_months(Variable):
     value_type = bool
     entity = Person
-    definition_period = YEAR
+    definition_period = MONTH
     label = u"Had previous parental leave in last six months"
-    reference = "http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM120450.html?search=sw_096be8ed8179b676_6+months_25_se&p=1"
+    reference = u"http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM120450.html?search=sw_096be8ed8179b676_previous_25_se&p=1&sr=1"
 
 
-class parental_leave__has_returned_to_work_except_KIT(Variable):
+class parental_leave__eligible_employee(Variable):
     value_type = bool
     entity = Person
-    definition_period = YEAR
-    label = u"Has returned to work (except for Keeping In Touch (KIT) hours)"
-    reference = "http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM6812627.html?search=sw_096be8ed8179b676_keeping+in+touch_25_se&p=1"
+    definition_period = MONTH
+    label = u"Eligible employee"
+    reference = u"http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM121539.html?search=sw_096be8ed8179b676_eligible_25_se&p=1"
+
+    def formula(persons, period, parameters):
+
+      return persons('parental_leave__is_primary_carer', period) * \
+        persons('parental_leave__applied_for_leave_or_stopped_working', period) * \
+        persons('parental_leave__passes_12_month_employment_test', period) * \
+        not_(persons('parental_leave__had_previous_parental_leave_in_last_six_months', period))
