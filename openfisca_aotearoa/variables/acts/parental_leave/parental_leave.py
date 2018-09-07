2c4e7e492ee558b2118db5779299fde22b843463
Dana Iti <dana.iti.work@gmail.com>
2018-09-07 18:17:12 +1200

    Added threshold tests

--- openfisca_aotearoa/variables/acts/parental_leave/parental_leave.py
+++ openfisca_aotearoa/variables/acts/parental_leave/parental_leave.py
@@ -21,19 +21,12 @@ class parental_leave__applied_for_leave_or_stopped_working(Variable):
     reference = "http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM121773.html"
 
 
-class parental_leave__passes_12_month_employment_test(Variable):
-    value_type = bool
+class parental_leave__threshold_tests(Variable):
+    value_type = int
     entity = Person
     definition_period = MONTH
-    label = u"Number of Persons classified as dependant for the purposes of rates rebates"
-    reference = """
-    http://www.legislation.govt.nz/act/public/1987/0129/latest/link.aspx
-
-    In this Act, the following tests are used to determine an employee’s entitlements to parental leave:
-    an employee meets the 12-month employment test if the employee will have been employed by the same employer for at least an average of 10 hours a week in the 12 months immediately preceding the expected date of—
-    (i) delivery of the child (in the case of a child to be born to the employee or to the employee’s spouse or partner); or
-    (ii) assumption of responsibility for the care of the child (in any other case).
-    """
+    label = u"Parental Leave Threshold Tests"
+    reference = u"http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM6810651.html"
 
 
 class parental_leave__had_previous_parental_leave_in_last_six_months(Variable):
@@ -56,5 +49,5 @@ class parental_leave__eligible_employee(Variable):
 
         return (is_citizen * persons('parental_leave__is_primary_carer', period) * \
             persons('parental_leave__applied_for_leave_or_stopped_working', period) * \
-            persons('parental_leave__passes_12_month_employment_test', period) * \
+            persons('parental_leave__threshold_tests', period) >= 6 * \
             not_(persons('parental_leave__had_previous_parental_leave_in_last_six_months', period)))
