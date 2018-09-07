222474f62ce1d205ddf573749c3c9b4b0b3099dd
Dana Iti <dana.iti.work@gmail.com>
2018-09-07 17:55:44 +1200

    Removed search str from urls, added tests for people who are not eligible and to check citizenship

--- openfisca_aotearoa/variables/acts/parental_leave/parental_leave.py
+++ openfisca_aotearoa/variables/acts/parental_leave/parental_leave.py
@@ -1,8 +1,7 @@
+
 # -*- coding: utf-8 -*-
 
-# Import from openfisca-core the common python objects used to code the legislation in OpenFisca
 from openfisca_core.model_api import *
-# Import the entities specifically defined for this tax and entitlement system
 from openfisca_aotearoa.entities import Person
 
 
@@ -11,7 +10,7 @@ class parental_leave__is_primary_carer(Variable):
     entity = Person
     definition_period = MONTH
     label = u"Is primary carer"
-    reference = "http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM120458.html?search=qs_act%40bill%40regulation%40deemedreg_parental+leave_resel_25_h&p=1#DLM120458"
+    reference = "http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM120458.html"
 
 
 class parental_leave__applied_for_leave_or_stopped_working(Variable):
@@ -19,7 +18,7 @@ class parental_leave__applied_for_leave_or_stopped_working(Variable):
     entity = Person
     definition_period = MONTH
     label = u"Has applied for/taken leave or stopped working immediately"
-    reference = "http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM121773.html?search=sw_096be8ed8179b676_immediately_25_se&p=1"
+    reference = "http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM121773.html"
 
 
 class parental_leave__passes_12_month_employment_test(Variable):
@@ -28,7 +27,7 @@ class parental_leave__passes_12_month_employment_test(Variable):
     definition_period = MONTH
     label = u"Number of Persons classified as dependant for the purposes of rates rebates"
     reference = """
-    http://www.legislation.govt.nz/act/public/1987/0129/latest/link.aspx?search=qs_act%40bill%40regulation%40deemedreg_parental+leave_resel_25_h&p=1&id=DLM6810651#DLM6810651
+    http://www.legislation.govt.nz/act/public/1987/0129/latest/link.aspx
 
     In this Act, the following tests are used to determine an employee’s entitlements to parental leave:
     an employee meets the 12-month employment test if the employee will have been employed by the same employer for at least an average of 10 hours a week in the 12 months immediately preceding the expected date of—
@@ -42,7 +41,7 @@ class parental_leave__had_previous_parental_leave_in_last_six_months(Variable):
     entity = Person
     definition_period = MONTH
     label = u"Had previous parental leave in last six months"
-    reference = u"http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM120450.html?search=sw_096be8ed8179b676_previous_25_se&p=1&sr=1"
+    reference = u"http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM120450.html"
 
 
 class parental_leave__eligible_employee(Variable):
@@ -50,10 +49,12 @@ class parental_leave__eligible_employee(Variable):
     entity = Person
     definition_period = MONTH
     label = u"Eligible employee"
-    reference = u"http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM121539.html?search=sw_096be8ed8179b676_eligible_25_se&p=1"
+    reference = u"http://www.legislation.govt.nz/act/public/1987/0129/latest/DLM121539.html"
 
     def formula(persons, period, parameters):
-        return persons('parental_leave__is_primary_carer', period) * \
+        is_citizen = persons('is_nz_citizen', period)
+
+        return (is_citizen * persons('parental_leave__is_primary_carer', period) * \
             persons('parental_leave__applied_for_leave_or_stopped_working', period) * \
             persons('parental_leave__passes_12_month_employment_test', period) * \
-            not_(persons('parental_leave__had_previous_parental_leave_in_last_six_months', period))
+            not_(persons('parental_leave__had_previous_parental_leave_in_last_six_months', period)))
