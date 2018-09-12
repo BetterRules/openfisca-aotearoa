f6efe3f3ce58647a55618c199724f92e19820d5f
Brenda Wallace <brenda@wallace.net.nz>
2018-09-12 20:48:10 +1200

    Tests the child is unsupported and nz resident

--- openfisca_aotearoa/variables/acts/social_security/29/unsupported_child.py
+++ openfisca_aotearoa/variables/acts/social_security/29/unsupported_child.py
@@ -12,10 +12,13 @@ class social_security__eligible_for_unsupported_childs_benefit(Variable):
     reference = "http://www.legislation.govt.nz/act/public/1964/0136/latest/whole.html#DLM361606"
     # Unsupported child’s benefit
 
-    # A person who is a principal caregiver in respect of a dependent child shall be entitled to receive an unsupported child’s benefit in respect of the child if—
+    # A person who is a principal caregiver in respect of a dependent child shall be entitled
+    #    to receive an unsupported child’s benefit in respect of the child if—
     # (a) that person is not the natural parent, adoptive parent, or step-parent of the child; and
-    # (b) because of a breakdown in the child’s family, no natural parent, adoptive parent, or step-parent of the child is able to care for the child or to provide fully for the child’s support; and
-    # (c) the applicant is likely to be the principal caregiver in respect of the child for at least 1 year from the date of application for the benefit; and
+    # (b) because of a breakdown in the child’s family, no natural parent, adoptive parent, or step-parent
+    #  of the child is able to care for the child or to provide fully for the child’s support; and
+    # (c) the applicant is likely to be the principal caregiver in respect of the child for at least
+    #  1 year from the date of application for the benefit; and
     # (d) the applicant is aged 18 years or over; and
     # (e) either—
     # (i) the child is both resident and present in New Zealand; or
@@ -30,7 +33,24 @@ class social_security__eligible_for_unsupported_childs_benefit(Variable):
             'social_security__is_principal_carer_for_one_year_from_application_date', period)
 
         is_principal_carer = persons.has_role(Family.PRINCIPAL_CAREGIVER)
-        has_a_child = persons.family(
-            "social_security__has_child_in_family", period)
 
-        return resident_or_citizen * not_the_parent * one_year * is_principal_carer * has_a_child
+        has_unsupported_child_in_family = persons(
+            'social_security__has_unsupported_child_in_family', period)
+
+        return resident_or_citizen * not_the_parent * one_year * is_principal_carer * has_unsupported_child_in_family
+
+
+class social_security__has_unsupported_child_in_family(Variable):
+    value_type = bool
+    entity = Family
+    definition_period = MONTH
+    label = u'each of the child’s natural or adoptive parents is dead, or cannot be found, or suffers a serious long-term disablement which renders him or her unable to care for the child'
+    reference = "http://www.legislation.govt.nz/act/public/1964/0136/latest/whole.html#DLM361606"
+
+    def formula(families, period, parameters):
+        children = families.members('social_security__is_a_child', period)
+        parents_unable = families.members(
+            'social_security__parents_unable_to_provide_sufficient_care', period)
+        resident_or_citizen = persons('is_citizen_or_resident', period)
+
+        return families.any((children * parents_unable * resident_or_citizen), role=Family.CHILD)
