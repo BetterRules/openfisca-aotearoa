39f6c982c8c2b48329f084264f4fc6aa7db87de5
Dana Iti <dana.iti.work@gmail.com>
2018-09-12 17:07:42 +1200

    Variables for Orphans benefit - WIP

--- openfisca_aotearoa/variables/acts/social_security/28/orphans_benefit.py
+++ openfisca_aotearoa/variables/acts/social_security/28/orphans_benefit.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-
-from openfisca_aotearoa.entities import Person
+from openfisca_aotearoa.entities import Person, Family
 from openfisca_core.model_api import *
 
 
@@ -8,18 +6,28 @@ class social_security__eligible_for_orphans_benefit(Variable):
     value_type = bool
     entity = Person
     definition_period = MONTH
-    label = "Eligible for Orphan's benefit"
-    reference = "http://www.legislation.govt.nz/act/public/1964/0136/latest/whole.html#DLM361606"
-    #  Orphans’ benefits
-
-    # A person who is a principal caregiver in respect of a dependent child shall be entitled to receive an orphan’s benefit in respect of that child if—
-    # (a) each of the child’s natural or adoptive parents is dead, or cannot be found, or suffers a serious long-term disablement which renders him or her unable to care for the child; and
-    # (b) the applicant is likely to be the principal caregiver in respect of the child for at least 1 year from the date of application for the benefit; and
-    # (c) the applicant is aged 18 years or over; and
-    # (d) either—
-    # (i) the child is both resident and present in New Zealand; or
-    # (ii) the applicant has been both resident and present in New Zealand for a continuous period of 12 months at any time.
+    label = u"Eligible for Orphan's benefit"
+    reference = """http://www.legislation.govt.nz/act/public/1964/0136/latest/whole.html#DLM361606
+        Orphans' benefits
+        A person who is a principal caregiver in respect of a dependent child shall be entitled to receive an orphan's benefit in respect of that child if
+        (a) each of the child's natural or adoptive parents is dead, or cannot be found, or suffers a serious long-term disablement which renders him or her unable to care for the child and
+        (b) the applicant is likely to be the principal caregiver in respect of the child for at least 1 year from the date of application for the benefit; and
+        (c) the applicant is aged 18 years or over; and
+        (d) either
+        (i) the child is both resident and present in New Zealand or
+        (ii) the applicant has been both resident and present in New Zealand for a continuous period of 12 months at any time.
+    """
 
     def formula(persons, period, parameters):
-        # todo
-        pass
+        resident_or_citizen = persons('is_citizen_or_resident', period)
+
+        not_the_parent = not_(
+            persons('social_security__is_the_parent_of_dependent_child', period))
+        one_year = persons(
+            'social_security__is_principal_carer_for_one_year_from_application_date', period)
+
+        is_principal_carer = persons.has_role(Family.PRINCIPAL_CAREGIVER)
+        has_a_child = persons.family(
+            "social_security__has_child_in_family", period)
+
+        return resident_or_citizen * not_the_parent * one_year * is_principal_carer * has_a_child
