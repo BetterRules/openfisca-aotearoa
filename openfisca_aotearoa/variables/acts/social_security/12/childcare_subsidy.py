f5e05f36b4665f9945bcc71aef6921771ab2b246
Brenda Wallace <brenda@wallace.net.nz>
2018-09-13 11:23:39 +1200

    Checks parent role and that child is dependent

--- openfisca_aotearoa/variables/acts/social_security/12/childcare_subsidy.py
+++ openfisca_aotearoa/variables/acts/social_security/12/childcare_subsidy.py
@@ -32,6 +32,8 @@ class social_security__eligible_for_childcare_subsidy(Variable):
 
     def formula(persons, period, parameters):
         is_citizen_or_resident = persons('is_citizen_or_resident', period)
+        is_principal_carer = persons.has_role(Family.PRINCIPAL_CAREGIVER)
+
 
         under_5_years_28_days_not_attending_school = persons.family(
             'family_has_resident_child_under_5_not_in_school', period)
@@ -39,7 +41,7 @@ class social_security__eligible_for_childcare_subsidy(Variable):
             'family_has_resident_child_aged_5_who_will_be_enrolled_in_school', period)
         under_6_with_disability_allowance = persons.family(
             'family_has_child_eligible_for_disability_allowance_child_under_6', period)
-        return is_citizen_or_resident * \
+        return is_citizen_or_resident * is_principal_carer * \
             (under_5_years_28_days_not_attending_school + is_5_and_will_be_enrolled + under_6_with_disability_allowance)
 
 
@@ -49,12 +51,13 @@ class family_has_resident_child_under_5_not_in_school(Variable):
     definition_period = MONTH
 
     def formula(families, period, parameters):
+        dependent_children = persons('is_dependent_child', period)
         not_in_school = not_(families.members(
             'is_attending_school', period))
         under_5 = families.members('age', period) < 5
         citizens_and_residents = families.members(
             'is_citizen_or_resident', period)
-        return families.any((citizens_and_residents * not_in_school * under_5), role=Family.CHILD)
+        return families.any((dependent_children * citizens_and_residents * not_in_school * under_5), role=Family.CHILD)
 
 
 class family_has_resident_child_aged_5_who_will_be_enrolled_in_school(Variable):
@@ -63,12 +66,13 @@ class family_has_resident_child_aged_5_who_will_be_enrolled_in_school(Variable):
     definition_period = MONTH
 
     def formula(families, period, parameters):
+        dependent_children = persons('is_dependent_child', period)
         children_to_be_enrolled = families.members(
             'will_be_enrolled_in_school', period)
         aged_5 = (families.members('age', period) == 5)
         citizens_and_residents = families.members(
             'is_citizen_or_resident', period)
-        return families.any((citizens_and_residents * children_to_be_enrolled * aged_5), role=Family.CHILD)
+        return families.any((dependent_children * citizens_and_residents * children_to_be_enrolled * aged_5), role=Family.CHILD)
 
 
 class family_has_child_eligible_for_disability_allowance_child_under_6(Variable):
@@ -77,9 +81,10 @@ class family_has_child_eligible_for_disability_allowance_child_under_6(Variable)
     definition_period = MONTH
 
     def formula(families, period, parameters):
+        dependent_children = persons('is_dependent_child', period)
         eligible_children = families(
             'disability_allowance__family_has_eligible_child', period)
         under_6 = families.members('age', period) < 6
         citizens_and_residents = families.members(
             'is_citizen_or_resident', period)
-        return families.any((citizens_and_residents * eligible_children * under_6), role=Family.CHILD)
+        return families.any((dependent_children * citizens_and_residents * eligible_children * under_6), role=Family.CHILD)
