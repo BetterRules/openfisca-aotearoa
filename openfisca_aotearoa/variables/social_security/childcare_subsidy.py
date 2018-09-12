01480047fe458c3a67dd643fdff906d212deb9bf
Dana Iti <dana.iti.work@gmail.com>
2018-09-12 15:16:32 +1200

    updated variables and tests, updated age variable

--- openfisca_aotearoa/variables/social_security/childcare_subsidy.py
+++ openfisca_aotearoa/variables/social_security/childcare_subsidy.py
@@ -13,11 +13,11 @@ class childcare_subsidy__is_a_dependant_child(Variable):
     reference = "http://www.legislation.govt.nz/regulation/public/2004/0268/latest/DLM282545.html"
 
 
-class childcare_subsidy__childs_age(Variable):
-    value_type = int
+class childcare_subsidy__child_is_no_older_than_5_years_28_days(Variable):
+    value_type = bool
     entity = Person
     definition_period = MONTH
-    label = u"Child's age"
+    label = u"Child is no older than 5 years 28_days"
     reference = "http://www.legislation.govt.nz/regulation/public/2004/0268/latest/DLM282545.html"
 
 
@@ -27,6 +27,16 @@ class childcare_subsidy__is_child_attending_school(Variable):
     definition_period = MONTH
     label = u"Is child attending school"
     reference = "http://www.legislation.govt.nz/regulation/public/2004/0268/latest/DLM282545.html"
+
+
+class childcare_subsidy__child_will_be_enrolled_to_school_with_cohort_policy(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = u"Child will be enrolled in a school that has a cohort entry policy in place"
+    reference = """ http://www.legislation.govt.nz/regulation/public/2004/0268/latest/DLM282545.html
+
+      (ba) who is 5, whose parent, principal caregiver, or guardian intends to enrol the child in a school that has a cohort entry policy in place, and who (under section 5B(2) of the Education Act 1989) may not be enrolled in that school until the term start date of the next term;"""
 
 
 class childcare_subsidy__child_has_disability_allowance(Variable):
@@ -47,7 +57,8 @@ class childcare_subsidy__eligibility(Variable):
     def formula(persons, period, parameters):
         is_citizen = persons('is_nz_citizen', period)
 
-        return is_citizen * persons('childcare_subsidy__is_primary_carer', period) * \
-            persons('childcare_subsidy__is_child_attending_school', period) * \
-            (persons('childcare_subsidy__childs_age', period) <= 5) + \
-            persons('childcare_subsidy__child_has_disability_allowance', period) * \
+        return is_citizen * persons('childcare_subsidy__is_a_dependant_child', period) * \
+            persons('childcare_subsidy__child_is_no_older_than_5_years_28_days', period) * \
+            not_(persons('childcare_subsidy__is_child_attending_school', period)) + \
+            persons('childcare_subsidy__child_will_be_enrolled_to_school_with_cohort_policy', period) + \
+            persons('childcare_subsidy__child_has_disability_allowance', period)
