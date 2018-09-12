5317922dc928d11097585e072cfe67d1d014fb15
Brenda Wallace <brenda@wallace.net.nz>
2018-09-12 16:24:53 +1200

    defined social_security__is_the_parent_of_dependent_child

--- openfisca_aotearoa/variables/acts/social_security/interpretation/child.py
+++ openfisca_aotearoa/variables/acts/social_security/interpretation/child.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Import from openfisca-core the common python objects used to code the legislation in OpenFisca
 from openfisca_core.model_api import *
-# Import the entities specifically defined for this tax and benefit system
 from openfisca_aotearoa.entities import Person
 
 
@@ -41,3 +39,9 @@ class social_security__has_child_in_family(Variable):
     def formula(persons, period, parameters):
         children = families.members('social_security__is_a_child', period)
         return families.any(children, role=Family.CHILD)
+
+
+class social_security__is_the_parent_of_dependent_child(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = ETERNITY
