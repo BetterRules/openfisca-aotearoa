71c48770f1775616199c1b8a92b60f746af951e8
Brenda Wallace <brenda@wallace.net.nz>
2018-09-12 22:54:19 +1200

    Moving the eligibility to the carer

--- openfisca_aotearoa/variables/acts/social_security/12/childcare_subsidy.py
+++ openfisca_aotearoa/variables/acts/social_security/12/childcare_subsidy.py
@@ -39,15 +39,7 @@ class childcare_subsidy__child_will_be_enrolled_to_school_with_cohort_policy(Var
       (ba) who is 5, whose parent, principal caregiver, or guardian intends to enrol the child in a school that has a cohort entry policy in place, and who (under section 5B(2) of the Education Act 1989) may not be enrolled in that school until the term start date of the next term;"""
 
 
-class childcare_subsidy__child_has_disability_allowance(Variable):
-    value_type = bool
-    entity = Person
-    definition_period = MONTH
-    label = u"Child has disability allowance"
-    reference = u"http://www.legislation.govt.nz/regulation/public/2004/0268/latest/DLM282545.html"
-
-
-class childcare_subsidy__eligibility(Variable):
+class social_security__eligible_for_childcare_subsidy(Variable):
     value_type = bool
     entity = Person
     definition_period = MONTH
@@ -57,8 +49,46 @@ class childcare_subsidy__eligibility(Variable):
     def formula(persons, period, parameters):
         is_citizen = persons('is_nz_citizen', period)
 
-        return is_citizen * persons('childcare_subsidy__is_a_dependant_child', period) * \
-            persons('childcare_subsidy__child_is_no_older_than_5_years_28_days', period) * \
-            not_(persons('childcare_subsidy__is_child_attending_school', period)) + \
-            persons('childcare_subsidy__child_will_be_enrolled_to_school_with_cohort_policy', period) + \
-            persons('childcare_subsidy__child_has_disability_allowance', period)
+        under_5_years_28_days_not_attending_school = persons.family(
+            'social_security__has_child_under_5_not_in_school', period)
+        is_5_and_will_be_enrolled = persons.family(
+            'social_security__has_child_aged_5_who_will_be_enrolled_in_school', period)
+        under_6_with_disability_allowance = persons.family(
+            'social_security__has_disability_allowance_child_under_6', period)
+        return is_citizen * (under_5_years_28_days_not_attending_school + is_5_and_will_be_enrolled + under_6_with_disability_allowance)
+
+
+class social_security__has_child_under_5_not_in_school(Variable):
+    value_type = bool
+    entity = Family
+    definition_period = MONTH
+
+    def formula(families, period, parameters):
+        not_in_school = families.members(
+            'childcare_subsidy__is_child_attending_school', period)
+        under_5 = families.members('age', period) < 5
+        return families.any((not_in_school * under_5), role=Family.CHILD)
+
+
+class social_security__has_child_aged_5_who_will_be_enrolled_in_school(Variable):
+    value_type = bool
+    entity = Family
+    definition_period = MONTH
+
+    def formula(families, period, parameters):
+        children_to_be_enrolled = families.members(
+            'childcare_subsidy__child_will_be_enrolled_to_school_with_cohort_policy', period)
+        aged_5 = families.members('age', period) == 5
+        return families.any((children_to_be_enrolled * aged_5), role=Family.CHILD)
+
+
+class social_security__has_disability_allowance_child_under_6(Variable):
+    value_type = bool
+    entity = Family
+    definition_period = MONTH
+
+    def formula(families, period, parameters):
+        eligible_children = families.members(
+            'social_security__family_has_eligible_disabled_child', period)
+        under_6 = families.members('age', period) < 6
+        return families.any((eligible_children * under_6), role=Family.CHILD)
