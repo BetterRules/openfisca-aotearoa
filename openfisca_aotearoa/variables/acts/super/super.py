1477af6b9f4f4733d15eb6c5031e35b9e7590f14
Dana Iti <dana.iti.work@gmail.com>
2018-10-09 16:26:24 +1300

    added more variables for super

--- openfisca_aotearoa/variables/acts/super/super.py
+++ openfisca_aotearoa/variables/acts/super/super.py
@@ -14,4 +14,36 @@ class super__living_alone(Variable):
     label = u"Is a person considered as living alone"
     reference = "http://www.legislation.govt.nz/act/public/2001/0084/latest/whole.html#DLM5578822"
 
+class super__age(Variable):
+    value_type = int
+    entity = Person
+    definition_period = MONTH
+    label = u"Applicants age"
+    reference = "http://www.legislation.govt.nz/act/public/2001/0084/latest/whole.html#DLM113987"
+
+class super__is_receiving_compensation(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = u"Is receiving compensation payment through ACC"
+    reference = "http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM105404.html#DLM105404"
+
+class super__has_partner(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = u"Has a partner"
+    reference = "http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM105404.html#DLM105404"
+
+class super__eligibility(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = u'Classified as eligible for NZ Super'
+    reference = "http://www.legislation.govt.nz/act/public/2001/0084/latest/DLM113985.html"
+
+    def formula(persons, period, parameters):
+        persons('super__age', period) +\
+        persons('super__is_receiving_compensation', period) +\
+        persons('super__has_partner', period)
 
