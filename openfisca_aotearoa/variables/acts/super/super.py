784fa1beb8631003088662c2996238a9746ecbe4
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-10-25 10:33:54 +1300

    Adding super___eligibility_age
    And removing unused variables

--- openfisca_aotearoa/variables/acts/super/super.py
+++ openfisca_aotearoa/variables/acts/super/super.py
@@ -1,25 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# Import from openfisca-core the common python objects used to code the legislation in OpenFisca
 from openfisca_core.model_api import *
-# Import the entities specifically defined for this tax and entitlement system
 from openfisca_aotearoa.entities import Person
 
 
-class super__living_alone(Variable):
+class super___eligibility_age(Variable):
     value_type = int
     entity = Person
-    definition_period = MONTH
-    label = u"Is a person considered as living alone"
-    reference = "http://www.legislation.govt.nz/act/public/2001/0084/latest/whole.html#DLM5578822"
-
-
-class super__has_partner_in_long_term_care_or_rest_home(Variable):
-    value_type = bool
-    entity = Person
-    definition_period = MONTH
-    label = u"Has a partner in long term care or rest home"
+    definition_period = ETERNITY
+    label = u"The age the applicant will be eligible for NZ Super."
     reference = "http://www.legislation.govt.nz/act/public/2001/0084/latest/DLM114223.html"
+
+    def formula(persons, period, parameters):
+        return persons('super__eligibility', period) * parameters(period).general.age_of_superannuation
 
 
 class super__eligibility(Variable):
@@ -33,6 +26,6 @@ class super__eligibility(Variable):
         return persons('is_citizen_or_resident', period) *\
             not_((persons('total_number_of_years_lived_in_nz_since_age_20', period) < 10)) *\
             not_((persons('total_number_of_years_lived_in_nz_since_age_50', period) < 5)) *\
-            (persons('age', period) >= 65) *\
             not_(persons('acc__is_receiving_compensation', period)) +\
-            persons('veterans_support__is_entitled_to_be_paid_veterans_pension', period)
+            persons(
+                'veterans_support__is_entitled_to_be_paid_veterans_pension', period)
