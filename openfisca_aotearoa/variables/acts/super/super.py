1f4d5227be8732edba45c21c2b3404f8ce1e7a4b
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-11-15 09:35:57 +1300

    Added formula to beneficaries

--- openfisca_aotearoa/variables/acts/super/super.py
+++ openfisca_aotearoa/variables/acts/super/super.py
@@ -29,3 +29,10 @@ class super__eligibility(Variable):
             not_(persons('acc__is_receiving_compensation', period)) +\
             persons(
                 'veterans_support__is_entitled_to_be_paid_veterans_pension', period)
+
+
+class super__is_being_paid_nz_superannuation(Variable):
+    value_type = bool
+    entity = Person
+    label = "New Zealand superannuation"
+    definition_period = MONTH
