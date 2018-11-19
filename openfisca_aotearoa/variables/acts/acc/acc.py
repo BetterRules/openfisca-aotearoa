d76c3b153cf329f40938358086f0bf301d633307
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-11-19 16:11:14 +1300

    Removed acc__elected_for_weekly_compensation, not used

--- openfisca_aotearoa/variables/acts/acc/acc.py
+++ openfisca_aotearoa/variables/acts/acc/acc.py
@@ -6,14 +6,6 @@ from openfisca_core.model_api import *
 from openfisca_aotearoa.entities import Person
 
 
-class acc__elected_for_weekly_compensation(Variable):
-    value_type = bool
-    entity = Person
-    definition_period = MONTH
-    label = u"Person eligible and elected to receive weekly compensation instead of superannuation"
-    reference = "http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM105440.html#DLM105440"
-
-
 class acc__is_receiving_compensation(Variable):
     value_type = bool
     entity = Person
