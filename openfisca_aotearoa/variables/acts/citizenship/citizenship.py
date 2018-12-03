a3f79ac7c5630462ef0ef88c2f4c0051a14d297d
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-12-03 17:04:43 +1300

    Tests for authorizing grant

--- openfisca_aotearoa/variables/acts/citizenship/citizenship.py
+++ openfisca_aotearoa/variables/acts/citizenship/citizenship.py
@@ -8,15 +8,16 @@ from openfisca_aotearoa.entities import Person
 class citizenship__citizenship_by_grant_may_be_authorized(Variable):
     value_type = bool
     entity = Person
-    definition_period = MONTH
+    definition_period = DAY
     reference = "http://www.legislation.govt.nz/act/public/1977/0061/latest/DLM443855.html"
 
     def formula_2005_04_20(persons, period, parameters):
-        return persons('age', period) >= parameters(period).citizenship.by_grant.minimum_age_threshold * \
+
+        return (persons('age', period) >= parameters(period).citizenship.by_grant.minimum_age_threshold) * \
             persons('is_of_full_capacity', period) * \
             persons('citizenship__meets_minimum_presence_requirements', period) * \
             persons('citizenship__is_of_good_character', period) * \
-            persons('citizenship__has_sufficient_knowledge_of_the_responsibilities_and_privileges_attaching_to_nz_citizenship') * \
+            persons('citizenship__has_sufficient_knowledge_of_the_responsibilities_and_privileges_attaching_to_nz_citizenship', period) * \
             persons('citizenship__has_sufficient_knowledge_of_the_english_language', period) * \
             (persons('citizenship__intends_to_reside_in_nz', period) + persons('citizenship__intends_to_enter_or_continue_crown_service', period))
 
@@ -65,15 +66,16 @@ class citizenship__meets_5_year_presence_requirement(Variable):
     entity = Person
     definition_period = DAY
 
-    label = u"was present in New Zealand for at least 240 days in each of those 5 years immediately preceding the date of the application"
+    label = u"was present in New Zealand for for a minimum of 1350 days during the 5 years immediately preceding the date of the application"
     reference = "http://www.legislation.govt.nz/act/public/1977/0061/latest/DLM443855.html"
 
     def formula(persons, period, parameters):
         # that the applicant was present in New Zealand—
-        # (i) for a minimum of 1350 days during the 5 years immediately preceding the date of the application; and
+        # (i)  and
         required_days = parameters(period).citizenship.by_grant.minimum_days_present_in_preceeding_5_years
+        days_present = persons('days_present_in_new_zealand_in_preceeding_5_years', period)
 
-        return persons('days_present_in_new_zealand_in_preceeding_5_years', period) >= required_days
+        return (days_present >= required_days)
 
 
 class days_present_in_new_zealand_in_preceeding_5_years(Variable):
