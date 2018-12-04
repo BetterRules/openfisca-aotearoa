2ddb01b682305a74181ea3ba89eded5f8365c4fa
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-12-04 13:01:18 +1300

    Update students.py

--- openfisca_aotearoa/variables/regulation/student_allowance/students.py
+++ openfisca_aotearoa/variables/regulation/student_allowance/students.py
@@ -7,7 +7,7 @@ from openfisca_aotearoa.entities import Person
 class student_allowance__is_tertiary_student(Variable):
     value_type = bool
     entity = Person
-    label = u"a tertiary student"
+    label = u"is a tertiary student"
     reference = "http://legislation.govt.nz/regulation/public/1998/0277/latest/whole.html#DLM259980"
     definition_period = MONTH
     default_value = False
@@ -16,7 +16,7 @@ class student_allowance__is_tertiary_student(Variable):
 class student_allowance__is_secondary_student(Variable):
     value_type = bool
     entity = Person
-    label = u"a secondary student"
+    label = u"is a secondary student"
     reference = "http://legislation.govt.nz/regulation/public/1998/0277/latest/whole.html#DLM259943"
     definition_period = MONTH
     default_value = False
