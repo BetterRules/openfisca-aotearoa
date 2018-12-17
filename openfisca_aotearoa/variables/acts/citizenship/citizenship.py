8d9dd6e9c52b98b070def77a8c743c976d792dcb
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-12-17 15:52:04 +1300

    linting

--- openfisca_aotearoa/variables/acts/citizenship/citizenship.py
+++ openfisca_aotearoa/variables/acts/citizenship/citizenship.py
@@ -115,12 +115,12 @@ def days_since_n_years_ago(day, n=1):
     """
     try:
         date_n_years_ago = day.replace(year=day.year - n)
-        #  The days in that rolling year could  be 365 or 366
-        days = (day - date_n_years_ago).days - 1 #  subtract one to not include that day
+        # The days in that rolling year could  be 365 or 366
+        days = (day - date_n_years_ago).days - 1  # subtract one to not include that day
     except ValueError:
-        #  Usually means a leap day, so try from the next day (1 March)
+        # Usually means a leap day, so try from the next day (1 March)
         date_n_years_ago = (day + timedelta(days=1)).replace(year=day.year - n)
-        days = (day - date_n_years_ago).days #  no need to subtract one, we already did above
+        days = (day - date_n_years_ago).days  # no need to subtract one, we already did above
 
     return days
 
