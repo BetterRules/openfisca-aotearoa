2d59d503b68f47c34083b7143752db6a46bb21d2
Hamish <hamish@verb.co.nz>
2019-01-04 00:23:16 +1300

    Additional fixes to home help

--- openfisca_aotearoa/variables/other/home_help.py
+++ openfisca_aotearoa/variables/other/home_help.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from openfisca_core.model_api import *
-from openfisca_aotearoa.entities import Person
+from openfisca_aotearoa.entities import Person, Family
 
 
 class home_help__had_multiple_birth(Variable):
@@ -38,10 +38,14 @@ class home_help__eligible_for_home_help(Variable):
     def formula(persons, period, parameters):
         resident_or_citizen = persons('is_citizen_or_resident', period)
         in_nz = persons('social_security__is_ordinarily_resident_in_new_zealand', period)
-        is_primary_carer = persons('parental_leave__is_primary_carer', period)
 
-        return resident_or_citizen * in_nz * is_primary_carer *\
-            persons('home_help__had_multiple_birth', period) +\
-            persons('home_help__adopted_2_or_more_children', period) +\
-            persons('home_help__has_no_immediate_family', period) *\
-            persons('has_community_services_card', period)
+        return resident_or_citizen * in_nz * persons.has_role(Family.PRINCIPAL_CAREGIVER) *\
+            (
+                persons('home_help__had_multiple_birth', period) +\
+                persons('home_help__adopted_2_or_more_children', period) +\
+                (
+                    persons('has_dependent_child', period) * persons('home_help__has_no_immediate_family', period) *\
+                    (persons('has_community_services_card', period) + persons('social_security__eligible_for_community_services_card', period))
+                )
+            )
+
