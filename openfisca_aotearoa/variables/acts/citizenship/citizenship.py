a8ebb8829247cf6b643448a41bcd5548864a8cad
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-02-18 17:05:09 +1300

    Chasing off by one bug in citizenship rolling year

--- openfisca_aotearoa/variables/acts/citizenship/citizenship.py
+++ openfisca_aotearoa/variables/acts/citizenship/citizenship.py
@@ -54,10 +54,26 @@ class citizenship__meets_each_year_minimum_presence_requirements(Variable):
         meets_presence = True
 
         for n in range(0, 5):
-            n_years_ago = period.offset(days_since_n_years_ago(period.date, n * -1))
-            meets_presence_n_years_ago = (persons('days_present_in_new_zealand_in_preceeding_year', n_years_ago) >= required_days)
+            #print("Checking year", n, "ending on", period.date)
 
+            days_ago = days_since_n_years_ago(period.date, n)
+            #print("the day to end our rolling year on is (the day before)", days_ago, "days before", period.date)
+
+            day_n_years_ago = period.offset(days_ago *-1)
+            #print("day_n_years_ago", day_n_years_ago)
+
+
+            days_present = persons('days_present_in_new_zealand_in_preceeding_year', day_n_years_ago)
+            #print("days present on rolling year ending at", day_n_years_ago, "is", days_present)
+
+            meets_presence_n_years_ago = (days_present >= required_days)
+            
+            #print("Meets rquirement??", meets_presence_n_years_ago)
+
+            # Accumulate the each year
             meets_presence = meets_presence_n_years_ago * meets_presence
+
+            #print("======================")
 
         return meets_presence
 
@@ -100,6 +116,7 @@ class days_present_in_new_zealand_in_preceeding_5_years(Variable):
     def formula(persons, period, parameters):
 
         sum = 0
+        "\t\t** -> days_present_in_new_zealand_in_preceeding_5_years"
         for offset in range((days_since_n_years_ago(period.date, 5) * -1), 1):
             p = period.offset(offset)
             sum += (persons('present_in_new_zealand', p) * 1)
@@ -116,8 +133,10 @@ def days_since_n_years_ago(day, n=1):
     """
     try:
         date_n_years_ago = day.replace(year=day.year - n)
+        
+        #print("date_n_years_ago", day, " subtract", n, "-->", date_n_years_ago)
         # The days in that rolling year could  be 365 or 366
-        days = (day - date_n_years_ago).days - 1  # subtract one to not include that day
+        days = (day - date_n_years_ago).days  # subtract one to not include that day
     except ValueError:
         # Usually means a leap day, so try from the next day (1 March)
         date_n_years_ago = (day + timedelta(days=1)).replace(year=day.year - n)
@@ -138,8 +157,11 @@ class days_present_in_new_zealand_in_preceeding_year(Variable):
 
         sum = 0
 
-        for p in [period.offset(offset) for offset in range((days_since_n_years_ago(period.date) * -1), 1)]:
-            sum += (persons('present_in_new_zealand', p) * 1)
+        for p in [period.offset(offset) for offset in range((days_since_n_years_ago(period.date) * -1), 0)]:
+            was_present = persons('present_in_new_zealand', p)
+            sum += (was_present * 1)
+            
+            #print("day", p, "presence =", was_present, "sum =", sum)
 
         return sum
 
