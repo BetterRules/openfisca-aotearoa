1d6b7b4fe9563c579421b8bc36cdf0853761f797
Mischa Saunders <mischa.saunders@gmail.com>
2019-03-26 09:17:03 +1300

    change definition period for incapacity_for_employment__by_covered_injury

--- openfisca_aotearoa/variables/acts/acc/105/incapacitated.py
+++ openfisca_aotearoa/variables/acts/acc/105/incapacitated.py
@@ -17,6 +17,6 @@ class incapacity_for_employment__corporation_determination(Variable):
 class incapacity_for_employment__by_covered_injury(Variable):
     value_type = bool
     entity = Person
-    definition_period = DAY
+    definition_period = ETERNITY
     label = u"The incapacity is caused by the injury for which they have cover"
     reference = "http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM101462.html"
