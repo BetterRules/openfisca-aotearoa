03fefb28e110a38f7c5bdc4f757d0dfb05a1e359
Dana Iti <dana.iti.work@gmail.com>
2019-04-02 11:00:41 +1300

    Added new test cases

--- openfisca_aotearoa/variables/acts/acc/8/has_cover.py
+++ openfisca_aotearoa/variables/acts/acc/8/has_cover.py
@@ -9,6 +9,6 @@ from openfisca_aotearoa.entities import Person
 class acc__has_cover(Variable):
     value_type = bool
     entity = Person
-    definition_period = DAY
+    definition_period = ETERNITY
     label = u"Has cover for a personal injury"
     reference = "http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM100605.html"
