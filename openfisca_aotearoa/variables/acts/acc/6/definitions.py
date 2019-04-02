24f68882970342c1e000869fa26893c37205ee72
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-04-02 16:14:53 +1300

    Code formatting

--- openfisca_aotearoa/variables/acts/acc/6/definitions.py
+++ openfisca_aotearoa/variables/acts/acc/6/definitions.py
@@ -33,22 +33,21 @@ class acc__potential_earner(Variable):
         injury_month = date_of_injury.astype('datetime64[M]').astype(int) % 12 + 1
         injury_day = (date_of_injury - date_of_injury.astype('datetime64[M]') + 1).astype(int)\
 
-        injured_under_18 = ((injury_year < eighteenth_year) +\
-            ((injury_year == eighteenth_year) * (injury_month < eighteenth_month)) +\
-            ((injury_year == eighteenth_year) * (injury_month == eighteenth_month) * (injury_day < eighteenth_day))
-            ) > 0
+        injured_under_18 = ((injury_year < eighteenth_year)
+                            + ((injury_year == eighteenth_year) * (injury_month < eighteenth_month))
+                            + ((injury_year == eighteenth_year) * (injury_month == eighteenth_month) * (injury_day < eighteenth_day))
+                            ) > 0
 
         finish_date = persons('finish_date_of_full_time_study_training_bridging_18th_birthday', period)
         finish_year = finish_date.astype('datetime64[Y]').astype(int) + 1970
         finish_month = finish_date.astype('datetime64[M]').astype(int) % 12 + 1
         finish_day = (finish_date - finish_date.astype('datetime64[M]') + 1).astype(int)\
 
-        injured_while_studying = ((injury_year < finish_year) +\
-            ((injury_year == finish_year) * (injury_month < finish_month)) +\
-            ((injury_year == finish_year) * (injury_month == finish_month) * (injury_day <= finish_day))
-            ) > 0
+        injured_while_studying = ((injury_year < finish_year)
+                                  + ((injury_year == finish_year) * (injury_month < finish_month))
+                                  + ((injury_year == finish_year) * (injury_month == finish_month) * (injury_day <= finish_day))
+                                  ) > 0
 
         injured_over_18_in_study = logical_not(injured_under_18) * injured_while_studying
 
         return (injured_under_18 + injured_over_18_in_study) > 0
-
