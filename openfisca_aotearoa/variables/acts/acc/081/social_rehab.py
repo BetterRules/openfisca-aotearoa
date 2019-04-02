a4b635ff14c17de9cb4fe0899b57526718028521
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-04-02 14:37:18 +1300

    More key aspects

--- openfisca_aotearoa/variables/acts/acc/081/social_rehab.py
+++ openfisca_aotearoa/variables/acts/acc/081/social_rehab.py
@@ -4,6 +4,13 @@ from openfisca_core.model_api import *
 from openfisca_aotearoa.entities import Person
 
 
+class acc__is_entitled_to_aids_and_appliances(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    reference = "Section 81 & 82 http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM101425.html"
+
+
 class acc__is_entitled_to_attendant_care(Variable):
     value_type = bool
     entity = Person
@@ -11,26 +18,73 @@ class acc__is_entitled_to_attendant_care(Variable):
     reference = "Section 81 & 82 http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM101425.html"
 
     def formula(persons, period, parameters):
-        return (
+        return (acc__the_corporation_exercised_descretion_as_per_section_68_3
             persons('acc__has_a_covered_injury', period)
             * persons('acc_part_3__has_lodged_claim', period)
             * persons('acc__assessed_as_having_a_need_caused_by_this_covered_injury', period)
             * persons('acc__the_corporation_decides_to_provide_or_contribute_to_attendant_care', period)
+            * persons('acc__key_aspect_is_necessary_and_appropriate', period)
+            * persons('acc__key_aspect_is_of_the_quality_required_for_that_purpose', period)
             * (persons('acc__is_present_in_nz', period)
                + persons('acc__number_of_days_outside_nz', period) <= 28)
-               + persons('acc__the_corporation_exercised_descretion_as_per_section_68_3', period)) # TODO move 28 to a parameter
+            + persons('acc__the_corporation_exercised_descretion_as_per_section_68_3', period))  # TODO move 28 to a parameter
+
+
+class acc__is_entitled_to_child_care(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    reference = "Section 81 & 82 http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM101425.html"
+
+
+class acc__is_entitled_to_education_support(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    reference = "Section 81 & 82 http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM101425.html"
+
+
+class acc__is_entitled_to_home_help(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    reference = "Section 81 & 82 http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM101425.html"
+
+
+class acc__is_entitled_to_modifications_to_the_home(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    reference = "Section 81 & 82 http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM101425.html"
+
+
+class acc__is_entitled_to_training_for_indepence(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    reference = "Section 81 & 82 http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM101425.html"
+
+
+class acc__is_entitled_to_transport_for_indepence(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    reference = "Section 81 & 82 http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM101425.html"
 
 
 class acc__has_a_covered_injury(Variable):
     value_type = bool
     entity = Person
     definition_period = MONTH
+    label = "has an ACC covered injury"
 
-
+# IS this needed?
+# will we say really "No, you're not entitled because you've not lodged a claim"
 class acc__lodges_a_claim_for_entitlement(Variable):
     value_type = bool
     entity = Person
     definition_period = MONTH
+    label = "lodges a claim for entitilement"
 
 
 class acc__assessed_as_having_a_need_caused_by_this_covered_injury(Variable):
@@ -38,6 +92,7 @@ class acc__assessed_as_having_a_need_caused_by_this_covered_injury(Variable):
     entity = Person
     definition_period = MONTH
     reference = "Section 84 http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM101430.html"
+    label ="was assess as having a need caused by this covered injury"
 
     # (4)      # The matters to be taken into account in an assessment or reassessment include—
     #   (a)    # the level of independence a claimant had before suffering the personal injury:
@@ -51,3 +106,19 @@ class acc__assessed_as_having_a_need_caused_by_this_covered_injury(Variable):
     #   (i)    # in the case of a reassessment,—
     #     (i)  # whether any item that the Corporation provided for the purposes of social rehabilitation is in such a condition as to need replacing:
     #     (ii) # changes in the claimant’s condition or circumstances since the last assessment was undertaken.
+
+
+class acc__key_aspect_is_necessary_and_appropriate(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = "key aspect is necessary and appropriate for that purpose"
+    reference = "section 81 (4) (c) (iv) http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM101426.html"
+
+
+class acc__key_aspect_is_of_the_quality_required_for_that_purpose(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = "key aspect is of the quality required for that purpose"
+    reference = "section 81 (4) (c) (iv) http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM101426.html"
