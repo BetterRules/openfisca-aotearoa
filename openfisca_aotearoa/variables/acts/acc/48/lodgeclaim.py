557943c094235df325d3af8c8d68445a956966cd
Mischa Saunders <mischa.saunders@gmail.com>
2019-04-04 14:30:14 +1300

    start adding purpose and lodge claim

--- openfisca_aotearoa/variables/acts/acc/48/lodgeclaim.py
+++ openfisca_aotearoa/variables/acts/acc/48/lodgeclaim.py
@@ -12,3 +12,14 @@ class acc__part_3__has_lodged_claim(Variable):
     definition_period = ETERNITY
     label = u"Has lodged a claim with the Corporation"
     reference = "http://www.legislation.govt.nz/act/public/2001/0049/latest/DLM100910.html"
+
+    def formula(persons, period, parameters):
+        return (
+            persons('acc__lodged_claim_for_cover_for_personal_injury', period)
+            + persons('acc__lodged_claim_for_cover_and_specified_entitlement_for_personal_injury', period)
+            + persons('acc__assessed_as_having_a_need_caused_by_this_covered_injury', period)
+        )
+
+# - a specified entitlement for his or her personal injury, once the Corporation has accepted the person has cover for the personal injury.
+
+# class acc__(Variable):
